846d5778e182c9ff25393feedbbea2e9b09fa5b3
Marco <rubolini.marco@gmail.com>
2020-04-21 18:13:06 +0200

    Esercizio mail

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,14 @@
 
+//Creo un array con le email autorizzate
+var iscritti = ["mario.rossi@gmail.com", "mario.verdi@gmail.com", "mario.gialli@gmail.com" ];
+console.log(iscritti);
+//"Sono in lista?" Chiedi all'utente la sua email
+var email = prompt('Inserisci la tua mail');
+console.log(email);
+//Controlla che sia nella lista di chi può accedere (ergo: la mail è presente in un array contenente le email "autorizzate") e stampa un messaggio appropriato (sei in lista/non sei in lista)
+var verificaMail = iscritti.includes(email);
+if (verificaMail = iscritti.includes(email)) {
+    console.log('Sei in lista');
+} else {
+    console.log('Non sei in lista');
+}
